7a8a299ea1c28182e06e50b01d60a334a6d0f9a3
LucasBraganca <lucasbraganask8@gmail.com>
2020-10-16 14:04:18 -0300

    fixed test.py

--- test/test.py
+++ test/test.py
@@ -1,13 +1,15 @@
 import os
 import sys
 
+mypath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from src.vcd_parser import VcdParser
 
 vcd_parser = VcdParser()
-ss = vcd_parser.parse('out.vcd')
+ss = vcd_parser.parse(mypath+'/out.vcd')
 
 for s in ss.signals:
     print(ss.signals[s])
     print(ss.signals[s].getValues(ss.getMaxTimeStamp()))
+    print()
